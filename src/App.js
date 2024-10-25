776fd282b37aad0a15d469789a1d154a371884bb
amirulhamizan12 <amirulhamizann@gmail.com>
2024-10-26 03:45:38 +0800

    added image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import helloImage from './pexels-athena-2582937.jpg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello World!</h1>
+        <img src={helloImage} alt="Hello World" className="hello-image" />
       </header>
     </div>
   );
